3a4cd4866f937c5940e8e59530222a9a9d1a72cd
Faisal <mohammad.faisal@juspay.in>
2023-09-22 13:07:25 +0530

    Initial

---
+++ index.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+import chalk from 'chalk'
+import inquirer from 'inquirer'
+import gradient from 'gradient-string'
+import chalkAnimation from 'chalk-animation'
+import figlet from 'figlet'
+
+console.log(chalk.bgGreen('Hello from Juspay'))
+
+async function askMID() {
+	const answers = await inquirer.prompt({
+		name: 'MID',
+		type: 'input',
+		message: 'What is you MID?',
+	})
+}
+
+await askMID()
