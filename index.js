6c174119fea4390d9315252d21861baa1b7f1adf
Faisal <mohammad.faisal@juspay.in>
2023-09-22 13:08:48 +0530

    MID Prompt

--- index.js
+++ index.js
@@ -8,12 +8,16 @@ import figlet from 'figlet'
 
 console.log(chalk.bgGreen('Hello from Juspay'))
 
+let mid
+
 async function askMID() {
 	const answers = await inquirer.prompt({
 		name: 'MID',
 		type: 'input',
 		message: 'What is you MID?',
 	})
+
+	mid = answers.MID
 }
 
 await askMID()
