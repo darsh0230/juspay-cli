ab6b8654ad6658a53f9bc2242615a0b7168c5e4a
darsh0230 <darshss0230@gmail.com>
2023-09-24 01:22:10 +0530

    flutter-installer fix

--- bin/index.js
+++ bin/index.js
@@ -1,113 +1,113 @@
 #!/usr/bin/env node
 
-import chalk from 'chalk'
-import ora from 'ora'
-import inquirer from 'inquirer'
-import gradient from 'gradient-string'
-import chalkAnimation from 'chalk-animation'
-import figlet from 'figlet'
-import axios from 'axios'
-import { isAndroid, isIOS, isFlutter, isRNative } from './platformChecks.js'
+import chalk from "chalk"
+import ora from "ora"
+import inquirer from "inquirer"
+import gradient from "gradient-string"
+import chalkAnimation from "chalk-animation"
+import figlet from "figlet"
+import axios from "axios"
+import { isAndroid, isIOS, isFlutter, isRNative } from "./platformChecks.js"
 import {
-	androidInstaller,
-	flutterInstaller,
-	iOSInstaller,
-	rNativeInstaller,
-} from './installers.js'
-import { createSpinner } from 'nanospinner'
+  androidInstaller,
+  flutterInstaller,
+  iOSInstaller,
+  rNativeInstaller,
+} from "./installers.js"
+import { createSpinner } from "nanospinner"
 
 const sleep = (ms = 100) => new Promise((r) => setTimeout(r, ms))
 
 async function welcome() {
-	console.clear()
-	const heading = 'Juspay CLI'
-	figlet(heading, (err, data) => {
-		console.log(gradient.pastel.multiline(data))
-	})
-	await sleep()
+  console.clear()
+  const heading = "Juspay CLI"
+  figlet(heading, (err, data) => {
+    console.log(gradient.pastel.multiline(data))
+  })
+  await sleep()
 }
 
 async function getPlatform() {
-	const spinner = createSpinner('Analyzing the Project').start()
-	let platform
-	if (isAndroid()) platform = 'Android'
-	else if (isIOS()) platform = 'iOS'
-	else if (isFlutter()) platform = 'Flutter'
-	else if (isRNative()) platform = 'React Native'
-	else platform = ''
-	if (platform != '')
-		spinner.success({
-			text: `${chalk.bold('Detected Project:')} ${chalk.rgb(
-				0,
-				200,
-				200
-			)(platform)}`,
-		})
-	else {
-		spinner.error({
-			text: 'Please run this CLI in the root directory of your project',
-		})
-		process.exit()
-	}
-	return platform.toLowerCase() == 'react native'
-		? 'rnative'
-		: platform.toLowerCase()
+  const spinner = createSpinner("Analyzing the Project").start()
+  let platform
+  if (isAndroid()) platform = "Android"
+  else if (isIOS()) platform = "iOS"
+  else if (isFlutter()) platform = "Flutter"
+  else if (isRNative()) platform = "React Native"
+  else platform = ""
+  if (platform != "")
+    spinner.success({
+      text: `${chalk.bold("Detected Project:")} ${chalk.rgb(
+        0,
+        200,
+        200
+      )(platform)}`,
+    })
+  else {
+    spinner.error({
+      text: "Please run this CLI in the root directory of your project",
+    })
+    process.exit()
+  }
+  return platform.toLowerCase() == "react native"
+    ? "rnative"
+    : platform.toLowerCase()
 }
 
 async function askMID() {
-	const answers = await inquirer.prompt({
-		name: 'MID',
-		type: 'input',
-		message: 'Enter the Merchant ID:',
-		validate: checkMid,
-	})
-	return answers.MID.toLowerCase()
+  const answers = await inquirer.prompt({
+    name: "MID",
+    type: "input",
+    message: "Enter the Merchant ID:",
+    validate: checkMid,
+  })
+  return answers.MID.toLowerCase()
 }
 
 async function checkMid(input) {
-	const url = `https://assets.juspay.in/hyper/bundles/in.juspay.merchants/${input.toLowerCase()}/${
-		platform == 'flutter' || platform == 'rnative' ? 'android' : platform
-	}/release/config.json`
-	try {
-		const res = await axios.get(url)
-		if (res.status == 200) return true
-	} catch (e) {}
-	return `${chalk.redBright.bold(
-		'Invalid Merchant ID'
-	)} (Make sure you're connected to the internet)`
+  const url = `https://assets.juspay.in/hyper/bundles/in.juspay.merchants/${input.toLowerCase()}/${
+    platform == "flutter" || platform == "rnative" ? "android" : platform
+  }/release/config.json`
+  try {
+    const res = await axios.get(url)
+    if (res.status == 200) return true
+  } catch (e) {}
+  return `${chalk.redBright.bold(
+    "Invalid Merchant ID"
+  )} (Make sure you're connected to the internet)`
 }
 
 async function askProduct() {
-	const answers = await inquirer.prompt({
-		name: 'product_select',
-		type: 'list',
-		message: 'Which Product are you integrating with?',
-		choices: ['Hyper Checkout', 'Express Checkout'],
-		loop: true,
-	})
+  const answers = await inquirer.prompt({
+    name: "product_select",
+    type: "list",
+    message: "Which Product are you integrating with?",
+    choices: ["Hyper Checkout", "Express Checkout"],
+    loop: true,
+  })
 
-	return answers.product_select
+  return answers.product_select
 }
 
 function install(mid, product) {
-	switch (platform) {
-		case 'android':
-			androidInstaller(mid, product)
-			break
-		case 'ios':
-			iOSInstaller(mid, product)
-			break
-		case 'flutter':
-			flutterInstaller(mid, product)
-			break
-		case 'rnative':
-			rNativeInstaller(mid, product)
-			break
-		default:
-			console.log(
-				chalk.redBright.bold('Caution: ') + 'Not in the root directory'
-			)
-	}
+  switch (platform) {
+    case "android":
+      androidInstaller(mid, product)
+      break
+    case "ios":
+      iOSInstaller(mid, product)
+      break
+    case "flutter":
+      flutterInstaller(mid, product)
+      break
+    case "rnative":
+      rNativeInstaller(mid, product)
+      break
+    default:
+      console.log(
+        chalk.redBright.bold("Caution: ") + "Not in the root directory"
+      )
+  }
 }
 
 await welcome()
--- bin/installers.js
+++ bin/installers.js
@@ -1,67 +1,84 @@
-import { createSpinner } from 'nanospinner'
-import { exec } from 'child_process'
-import fs from 'fs'
-import { addToBuildGradle, addToJSON, addToPodfile, pubGetSDK } from './utils.js'
-import chalk from 'chalk'
+import { createSpinner } from "nanospinner"
+import { exec } from "child_process"
+import fs from "fs"
+import {
+  addToBuildGradle,
+  addToJSON,
+  addToPodfile,
+  flutterAddToGradle,
+  flutterAddToPodfile,
+  pubGetSDK,
+} from "./utils.js"
+import chalk from "chalk"
 
 export const androidInstaller = () => {
-	const paths = {
-		'build.gradle': './android/build.gradle',
-	}
+  const paths = {
+    "build.gradle": "./android/build.gradle",
+    "app.build.gradle": "./android/app/build.gradle",
+  }
 }
 
 export const iOSInstaller = () => {}
 
 export const flutterInstaller = (mid) => {
-	const paths = {
-		'build.gradle': './android/build.gradle',
-		podfile: './ios/Podfile',
-		merchantConfig: './ios/MerchantConfig.txt',
-	}
+  const spinner = createSpinner("Setting up your project").start()
+  const paths = {
+    "build.gradle": "./android/build.gradle",
+    podfile: "./ios/Podfile",
+    merchantConfig: "./ios/MerchantConfig.txt",
+  }
 
-	// install hypersdkflutter
-	pubGetSDK()
-	// add the script to build gradle
-	addToBuildGradle(paths['build.gradle'], mid)
-	// add the script to pod file
-	addToPodfile(paths['podfile'])
-	// add the merchant config file
-	fs.writeFileSync(paths['merchantConfig'], `clientId = ${mid}`, (err) =>
-		console.log(err)
-	)
+  // install hypersdkflutter
+  pubGetSDK()
+  // add the script to build gradle
+  flutterAddToGradle(paths["build.gradle"], mid)
+  // check and add the script to pod file
+  if (!fs.existsSync(paths["podfile"])) {
+    spinner.warn({
+      text: chalk.yellowBright.bold(
+        "Missing podfile : Looks like the ios build is not initiated yet. Kindly initiate the build and run the juspay-cli again."
+      ),
+    })
+    return
+  }
+  flutterAddToPodfile(paths["podfile"])
+  // add the merchant config file
+  fs.writeFileSync(paths["merchantConfig"], `clientId = ${mid}`, (err) =>
+    console.log(err)
+  )
 
-	// success
-	spinner.success({
-		text: chalk.greenBright.bold(
-			'Your project has been set up and ready to go 🎉'
-		),
-	})
+  // success
+  spinner.success({
+    text: chalk.greenBright.bold(
+      "Your project has been set up and ready to go 🎉"
+    ),
+  })
 }
 
 export const rNativeInstaller = (mid, product) => {
-	const paths = {
-		'build.gradle': './android/build.gradle',
-		'package.json': './package.json',
-		Podfile: './ios/Podfile',
-		merchantConfig: './ios/MerchantConfig.txt',
-	}
-	const spinner = createSpinner('Setting up your project').start()
-	const packageName = 'hyper-sdk-react'
-	addToJSON(paths['package.json'], 'hyperSdkIOSVersion', '2.1.23')
-	addToBuildGradle(paths['build.gradle'], mid)
-	fs.writeFileSync(paths['merchantConfig'], `clientId = ${mid}`, (err) =>
-		console.log(err)
-	)
-	addToPodfile(paths['Podfile'])
-	exec(`npm install ${packageName}`, (error) => {
-		if (error) {
-			spinner.error({ text: `Error: ${error.message}` })
-			return
-		}
-		spinner.success({
-			text: chalk.greenBright.bold(
-				'Your project has been set up and ready to go 🎉'
-			),
-		})
-	})
+  const paths = {
+    "build.gradle": "./android/build.gradle",
+    "package.json": "./package.json",
+    Podfile: "./ios/Podfile",
+    merchantConfig: "./ios/MerchantConfig.txt",
+  }
+  const spinner = createSpinner("Setting up your project").start()
+  const packageName = "hyper-sdk-react"
+  addToJSON(paths["package.json"], "hyperSdkIOSVersion", "2.1.23")
+  addToBuildGradle(paths["build.gradle"], mid)
+  fs.writeFileSync(paths["merchantConfig"], `clientId = ${mid}`, (err) =>
+    console.log(err)
+  )
+  addToPodfile(paths["Podfile"])
+  exec(`npm install ${packageName}`, (error) => {
+    if (error) {
+      spinner.error({ text: `Error: ${error.message}` })
+      return
+    }
+    spinner.success({
+      text: chalk.greenBright.bold(
+        "Your project has been set up and ready to go 🎉"
+      ),
+    })
+  })
 }
